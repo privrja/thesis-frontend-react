6f596f37fb99d4a19f9487bd5631219885fdf8a8
privrja <privrja@gmail.com>
2021-01-10 13:44:16 +0100

    Logout

--- src/page/LogoutPage.tsx
+++ src/page/LogoutPage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Flash from "../component/Flash";
 import FlashType from "../component/FlashType";
 import {TOKEN} from "../constant/ApiConstants";
+import Sleep from "../helper/Sleep";
 
 class LogoutPage extends React.Component<any> {
 
@@ -20,12 +21,14 @@ class LogoutPage extends React.Component<any> {
     logout() {
         localStorage.removeItem(TOKEN);
         this.flashRef.current!.activate(FlashType.OK);
+        Sleep.sleep(500).then(() => {
+            window.location.href = '/'
+        });
     }
 
     render() {
         return <Flash textBad='Logout failure!' textOk='Logout successful!' ref={this.flashRef}/>
     }
-
 
 }
 
