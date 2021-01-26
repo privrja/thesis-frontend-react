2ef033fd48275e7fb74b03fdaaa0ca9321914077
privrja <privrja@gmail.com>
2021-01-26 17:01:59 +0100

    Modifications show/hide

--- src/component/ModificationInput.tsx
+++ src/component/ModificationInput.tsx
@@ -4,7 +4,7 @@ import ModificationSelect from "./ModificationSelect";
 import {SelectOption} from "./SelectInput";
 import Modification from "../structure/Modification";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faPlus, faMinus, faBan} from '@fortawesome/free-solid-svg-icons'
+import {faPlus, faMinus} from '@fortawesome/free-solid-svg-icons'
 
 interface Props {
     type: string;
@@ -81,44 +81,50 @@ class ModificationInput extends React.Component<Props, State> {
 
     render() {
         return (
-            <div className="div-modification">
-                <h4>{this.props.title} {this.state.isDisabled ? <FontAwesomeIcon icon={faBan}/> : this.state.isVisible ?
-                    <FontAwesomeIcon icon={faMinus} onClick={this.deactivate} className={styles.cursorPointer}/> :
-                    <FontAwesomeIcon icon={faPlus} onClick={this.activate} className={styles.cursorPointer}/>}</h4>
+            !this.state.isDisabled ?
 
-                {this.state.isVisible ?
+                <div className="div-modification">
+                    <h4>{this.props.title} {this.state.isVisible ?
+                        <FontAwesomeIcon icon={faMinus} onClick={this.deactivate} className={styles.cursorPointer}/> :
+                        <FontAwesomeIcon icon={faPlus} onClick={this.activate} className={styles.cursorPointer}/>}</h4>
 
-                    <div id={'div-' + this.props.type + MODIFICATION} className={styles.divLeft}>
+                    {this.state.isVisible ?
 
-                        {this.props.modifications && this.props.modifications.length > 1 ?
-                            <ModificationSelect id={'sel-' + this.props.type + MODIFICATION}
-                                                name={'sel-' + this.props.type + MODIFICATION}
-                                                modifications={this.props.modifications}
-                                                defaultOption={new SelectOption('nothing', 'Add new')}
-                                                onChange={this.select}/>
-                            : <div/>
-                        }
+                        <div id={'div-' + this.props.type + MODIFICATION} className={styles.divLeft}>
 
-                        <label htmlFor={'txt-' + this.props.type + MODIFICATION}>Name</label>
-                        <input type="text" id={'txt-' + this.props.type + MODIFICATION}
-                               name={this.props.type + 'Modification'} value=""/>
+                            {this.props.modifications && this.props.modifications.length > 1 ?
+                                <ModificationSelect id={'sel-' + this.props.type + MODIFICATION}
+                                                    name={'sel-' + this.props.type + MODIFICATION}
+                                                    modifications={this.props.modifications}
+                                                    defaultOption={new SelectOption('nothing', 'Add new')}
+                                                    onChange={this.select}/>
+                                : <div/>
+                            }
 
-                        <label htmlFor={'txt-' + this.props.type + FORMULA}>Formula</label>
-                        <input type="text" id={'txt-' + this.props.type + FORMULA} name={this.props.type + 'Formula'}
-                               value=""/>
+                            <label htmlFor={'txt-' + this.props.type + MODIFICATION}>Name</label>
+                            <input type="text" id={'txt-' + this.props.type + MODIFICATION}
+                                   name={this.props.type + 'Modification'} value=""/>
 
-                        <label htmlFor={'txt-' + this.props.type + MASS}>Monoisotopic Mass</label>
-                        <input type="text" id={'txt-' + this.props.type + MASS} name={this.props.type + 'Mass'}
-                               value=""/>
+                            <label htmlFor={'txt-' + this.props.type + FORMULA}>Formula</label>
+                            <input type="text" id={'txt-' + this.props.type + FORMULA}
+                                   name={this.props.type + 'Formula'}
+                                   value=""/>
 
-                        <label htmlFor={'chk-' + this.props.type + NTERMINAL} className="chk">N-terminal</label>
-                        <input type="checkbox" id={'chk-' + this.props.type + NTERMINAL} name={this.props.type + 'nTerminal'} checked={this.props.type === 'n'}/>
+                            <label htmlFor={'txt-' + this.props.type + MASS}>Monoisotopic Mass</label>
+                            <input type="text" id={'txt-' + this.props.type + MASS} name={this.props.type + 'Mass'}
+                                   value=""/>
 
-                        <label htmlFor={'chk-' + this.props.type + CTERMINAL} className="chk">C-terminal</label>
-                        <input type="checkbox" id={'chk-' + this.props.type + CTERMINAL} name={this.props.type + 'cTerminal'} checked={this.props.type === 'c'}/>
-                    </div>
-                    : <div/>}
-            </div>
+                            <label htmlFor={'chk-' + this.props.type + NTERMINAL} className="chk">N-terminal</label>
+                            <input type="checkbox" id={'chk-' + this.props.type + NTERMINAL}
+                                   name={this.props.type + 'nTerminal'} checked={this.props.type === 'n'}/>
+
+                            <label htmlFor={'chk-' + this.props.type + CTERMINAL} className="chk">C-terminal</label>
+                            <input type="checkbox" id={'chk-' + this.props.type + CTERMINAL}
+                                   name={this.props.type + 'cTerminal'} checked={this.props.type === 'c'}/>
+                        </div>
+                        : <div/>}
+                </div>
+                : <div/>
         );
     }
 }
