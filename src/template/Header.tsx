4c678ee02ef0a6ff3bacc5e6b210a0dac1ca163e
privrja <privrja@gmail.com>
2021-03-17 16:44:12 +0100

    Hide menu

--- src/template/Header.tsx
+++ src/template/Header.tsx
@@ -14,8 +14,8 @@ class Header extends React.Component {
                     <HeaderTile text={'Sequences'} url={URL_PREFIX + 'container/' + localStorage.getItem(SELECTED_CONTAINER ?? '4') + '/sequence'}/>
                     <HeaderTile text={'Blocks'} url={URL_PREFIX + 'container/' + (localStorage.getItem(SELECTED_CONTAINER) ?? '4') + '/block'}/>
                     <HeaderTile text={'Modifications'} url={URL_PREFIX + 'container/'+ (localStorage.getItem(SELECTED_CONTAINER) ?? '4') + '/modification'}/>
-                    <HeaderTile text={'Import'} url={URL_PREFIX + 'import'}/>
-                    <HeaderTile text={'Settings'} url={URL_PREFIX + 'setup'}/>
+                    {localStorage.getItem(TOKEN) ? <HeaderTile text={'Import'} url={URL_PREFIX + 'import'}/>: '' }
+                    {localStorage.getItem(TOKEN) ? <HeaderTile text={'Settings'} url={URL_PREFIX + 'setup'}/> : ''}
                     {localStorage.getItem(TOKEN) ? <HeaderTile text={'Logout'} url={URL_PREFIX + 'logout'}/> : <HeaderTile text={'Login'} url={URL_PREFIX + 'login'}/>}
                 </div>
             </header>
