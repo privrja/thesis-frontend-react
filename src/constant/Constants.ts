8e25720e859a290950c7e43d59f560c3d3527147
privrja <privrja@gmail.com>
2021-04-13 17:04:32 +0200

    Deployment

--- src/constant/Constants.ts
+++ src/constant/Constants.ts
@@ -1,6 +1,6 @@
 
 export const ENDPOINT = 'https://ms.biomed.cas.cz/msb-backend/public/index.php/rest/';
-export const URL_PREFIX = './msb';
+export const URL_PREFIX = '';
 export const DECIMAL_PLACES = 6;
 export const SHOW_ID = false;
 export const CHEMSPIDER_ONE_KEY : string = '';
