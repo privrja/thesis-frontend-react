18e2c1ec16e22ddb066b44862b4aab3a8ce1657f
privrja <privrja@gmail.com>
2021-04-15 09:17:54 +0200

    Table width

--- src/constant/Constants.ts
+++ src/constant/Constants.ts
@@ -1,5 +1,5 @@
 
-export const ENDPOINT = 'https://localhost:8000/rest/';
+export const ENDPOINT = 'https://ms.biomed.cas.cz/msb-backend/public/index.php/rest/';
 export const URL_PREFIX = '/';
 export const DECIMAL_PLACES = 6;
 export const SHOW_ID = false;
