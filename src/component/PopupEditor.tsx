b159ec4f1e1177328e2be7679d63f67cadaec699
privrja <privrja@gmail.com>
2021-04-25 08:24:37 +0200

    Gramicidin problem

--- src/component/PopupEditor.tsx
+++ src/component/PopupEditor.tsx
@@ -57,7 +57,7 @@ class PopupEditor extends React.Component<Props, State> {
         if (this.state.isActive) {
             return (
                 <div className={this.props.className}>
-                    <Jsme height="80vh" width="50wv" options="oldlook,star" smiles={this.state.firstSmiles}
+                    <Jsme height="80vh" width="50wv" options="oldlook,star,nocanonize" smiles={this.state.firstSmiles}
                           onChange={this.smiles}/>
                     <div>
                         <button onClick={this.done}>Done</button>
